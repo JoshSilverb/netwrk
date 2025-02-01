799e83efe13c9fb61956ef828244cf821f74434a
Josh <joshsilverberg8@gmail.com>
2025-01-31 19:19:38 -0500

    Add coords column and tags + socials tables to DB

--- backend/sql/data.sql
+++ backend/sql/data.sql
@@ -1,18 +1,77 @@
-INSERT INTO users (username, password) VALUES ('josh', 'asdf');
+INSERT INTO users (username, password) VALUES ('josh', '$2b$12$72O1dSQ1/x7.ulYhVznk/uTpfDc8r9WmPqI0tgAQMayAjd2ZJ3vwS');
 
-INSERT INTO contacts (user_id, fullname, location, emailaddress, phonenumber, userbio) VALUES
-(1, 'Alice Allison', 'San Francisco', 'alice@email.com', '123456789', 'Alice is a software engineer in the bay'),
-(1, 'Bob Bobby', 'New York', 'bob@bobbymail.com', '987654321', 'Bob works in finance and enjoys baseball.'),
-(1, 'Cam Smith', 'Chicago', 'cam@email.com', '555555555', 'Cam does contracting and plays a lot of baseball which he really likes'),
-(1, 'Big Dawg', 'New York', 'big@dawg.com', '6666666666', 'Gig concert guitarist'),
-(1, 'David Daniels', 'Los Angeles', 'david.daniels@mail.com', '7777777777', 'David is a filmmaker and loves surfing.'),
-(1, 'Evelyn Edwards', 'Miami', 'evelyn.edwards@mail.com', '8888888888', 'Evelyn is a marine biologist who enjoys scuba diving.'),
-(1, 'Frank Foster', 'Austin', 'frank.foster@mail.com', '9999999999', 'Frank is a musician and a BBQ enthusiast.'),
-(1, 'Grace Green', 'Seattle', 'grace.green@mail.com', '1111111111', 'Grace is a software developer and an avid hiker.'),
-(1, 'Henry Hughes', 'Boston', 'henry.hughes@mail.com', '2222222222', 'Henry is a historian who enjoys reading and writing.'),
-(1, 'Ivy Ingram', 'Denver', 'ivy.ingram@mail.com', '3333333333', 'Ivy is a graphic designer who loves skiing.'),
-(1, 'Jack Johnson', 'Portland', 'jack.johnson@mail.com', '4444444444', 'Jack is a photographer and a nature enthusiast.'),
-(1, 'Katie King', 'San Diego', 'katie.king@mail.com', '5555555555', 'Katie is a chef who loves to explore new cuisines.'),
-(1, 'Leo Lewis', 'Las Vegas', 'leo.lewis@mail.com', '6666666666', 'Leo is a casino manager who enjoys poker and gaming.'),
-(1, 'Mia Martinez', 'Phoenix', 'mia.martinez@mail.com', '7777777777', 'Mia is a teacher who loves to paint and draw.'),
-(1, 'Nina Nelson', 'Dallas', 'nina.nelson@mail.com', '8888888888', 'Nina is a marketing professional who enjoys yoga and meditation.');
+INSERT INTO contacts (user_id, fullname, location, userbio, metthrough) VALUES
+(1, 'Alice Allison', 'San Francisco, CA, USA', 'Alice is a software engineer in the bay', 'met in college'),
+(1, 'Bob Bobby', 'New York, NY, USA', 'Bob works in finance and enjoys baseball.', 'met in college'),
+(1, 'Cam Smith', 'Chicago, IL, USA', 'Cam does contracting and plays a lot of baseball which he really likes', 'met in college'),
+(1, 'Big Dawg', 'New York, NY, USA', 'Gig concert guitarist', 'met in college'),
+(1, 'David Daniels', 'Los Angeles, CA, USA', 'David is a filmmaker and loves surfing.', 'met in college'),
+(1, 'Evelyn Edwards', 'Miami, FL, USA', 'Evelyn is a marine biologist who enjoys scuba diving.', 'met in college'),
+(1, 'Frank Foster', 'Austin, TX, USA', 'Frank is a musician and a BBQ enthusiast.', 'met in college'),
+(1, 'Grace Green', 'Seattle, WA, USA', 'Grace is a software developer and an avid hiker.', 'met in college'),
+(1, 'Henry Hughes', 'Boston, MA, USA', 'Henry is a historian who enjoys reading and writing.', 'met in college'),
+(1, 'Ivy Ingram', 'Denver, CO, USA', 'Ivy is a graphic designer who loves skiing.', 'met in college'),
+(1, 'Jack Johnson', 'Portland, OR, USA', 'Jack is a photographer and a nature enthusiast.', 'met in college'),
+(1, 'Katie King', 'San Diego, CA, USA', 'Katie is a chef who loves to explore new cuisines.', 'met in college'),
+(1, 'Leo Lewis', 'Las Vegas, NV, USA', 'Leo is a casino manager who enjoys poker and gaming.', 'met in college'),
+(1, 'Mia Martinez', 'Phoenix, AZ, USA', 'Mia is a teacher who loves to paint and draw.', 'met in college'),
+(1, 'Nina Nelson', 'Dallas, TX, USA', 'Nina is a marketing professional who enjoys yoga and meditation.', 'met in college');
+
+INSERT INTO sociallabels (label) VALUES
+('Email Address'),
+('Phone Number'),
+('LinkedIn'),
+('Instagram');
+
+INSERT INTO socials (contact_id, social_id, address) VALUES
+(1, 1, 'alice@email.com'),
+(1, 2, '123456789'),
+(2, 1, 'bob@bobbymail.com'),
+(2, 2, '987654321'),
+(3, 1, 'cam@email.com'),
+(3, 2, '6666666666'),
+(4, 1, 'big@dawg.com'),
+(4, 2, '555555555'),
+(5, 1, 'david.daniels@mail.com'),
+(5, 2, '7777777777'),
+(6, 1, 'evelyn.edwards@mail.com'),
+(6, 2, '8888888888'),
+(7, 1, 'frank.foster@mail.com'),
+(7, 2, '9999999999'),
+(8, 1, 'grace.green@mail.com'),
+(8, 2, '1111111111'),
+(9, 1, 'henry.hughes@mail.com'),
+(9, 2, '2222222222'),
+(10, 1, 'ivy.ingram@mail.com'),
+(10, 2, '3333333333'),
+(11, 1, 'jack.johnson@mail.com'),
+(11, 2, '4444444444'),
+(12, 1, 'katie.king@mail.com'),
+(12, 2, '5555555555'),
+(13, 1, 'leo.lewis@mail.com'),
+(13, 2, '6666666666'),
+(14, 1, 'mia.martinez@mail.com'),
+(14, 2, '7777777777'),
+(15, 1, 'nina.nelson@mail.com'),
+(15, 2, '8888888888');
+
+INSERT INTO taglabels (user_id, label) VALUES
+(1, 'Friends'),
+(1, 'Work');
+
+INSERT INTO tags (contact_id, tag_id) VALUES
+(1, 1),
+(2, 1),
+(3, 1),
+(4, 1),
+(5, 1),
+(6, 1),
+(7, 1),
+(8, 1),
+(9, 2),
+(10, 2),
+(11, 2),
+(12, 2),
+(13, 2),
+(14, 2),
+(15, 2);
--- backend/sql/schema.sql
+++ backend/sql/schema.sql
@@ -1,3 +1,6 @@
+CREATE EXTENSION vector;
+CREATE EXTENSION postgis;
+
 CREATE TABLE users(
     user_id    SERIAL PRIMARY KEY UNIQUE,
     user_token CHAR(33), -- UUID populated upon successful login. Used for identity verification
@@ -6,36 +9,47 @@ CREATE TABLE users(
     num_contacts INTEGER DEFAULT 0  -- number of contacts user has
 );
 
-CREATE EXTENSION vector;
-
 CREATE TABLE contacts(
     contact_id SERIAL PRIMARY KEY UNIQUE,
     user_id INTEGER NOT NULL,
     fullname VARCHAR(128) NOT NULL,
     location VARCHAR(128),
-    emailaddress VARCHAR(64) CHECK (emailaddress LIKE '%@%.%'),
-    phonenumber VARCHAR(32),
-    linkedin VARCHAR(64),
-    instagram VARCHAR(64),
+    coordinates GEOGRAPHY(Point, 4326), -- better way to store coords: https://chatgpt.com/c/679b3589-d888-800f-a885-17aa44789b54
     metthrough VARCHAR(256),
     userbio VARCHAR(500),
-    lastcontact DATE,
+    lastcontact DATE DEFAULT CURRENT_DATE,
     importance INTEGER,
     embedding vector(784),
     FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE
 );
+-- Index on contacts.coordinates to make location lookup quicker
+CREATE INDEX locations_gix ON contacts USING GIST (coordinates);
+
 -- do search just on userbio for now
 
--- FUTURE WORK:
--- CREATE TABLE socialmedia(
---     contact_id INTEGER NOT NULL,
---     label VARCHAR(32),
---     address VARCHAR(64),
---     FOREIGN KEY(contact_id) REFERENCES contacts(contact_id)
--- )
+CREATE TABLE sociallabels(
+    id SERIAL PRIMARY KEY UNIQUE,
+    label VARCHAR(16)
+);
 
--- CREATE TABLE tags(
---     contact_id INTEGER NOT NULL,
---     tag VARCHAR(32),
---     FOREIGN KEY(contact_id) REFERENCES contacts(contact_id) ON DELETE CASCADE
--- )
+CREATE TABLE socials(
+    contact_id INTEGER NOT NULL,
+    social_id INTEGER NOT NULL,
+    address VARCHAR(64),
+    FOREIGN KEY(contact_id) REFERENCES contacts(contact_id),
+    FOREIGN KEY(social_id) REFERENCES sociallabels(id)
+);
+
+CREATE TABLE taglabels(
+    id SERIAL PRIMARY KEY UNIQUE,
+    user_id INTEGER NOT NULL,
+    label VARCHAR(16),
+    FOREIGN KEY(user_id) REFERENCES users(user_id) ON DELETE CASCADE
+);
+
+CREATE TABLE tags(
+    contact_id INTEGER NOT NULL,
+    tag_id INTEGER NOT NULL,
+    FOREIGN KEY(contact_id) REFERENCES contacts(contact_id) ON DELETE CASCADE,
+    FOREIGN KEY(tag_id) REFERENCES taglabels(id) ON DELETE CASCADE
+);
