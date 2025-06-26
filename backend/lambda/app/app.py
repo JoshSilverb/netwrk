114aa6ae21f4492c5a02bbd7a3f495b13f1f9ae7
Josh Silverberg <joshsiverberg8@gmail.com>
2025-06-25 23:50:37 -0400

    Delete old lambda code

--- backend/lambda/app/app.py
+++
@@ -1,8 +0,0 @@
-from app.request_processor import RequestProcessor
-
-def handle_request(event, context):
-    requestProcessor = RequestProcessor()
-
-    response = requestProcessor.route_request(event, context)
-
-    return response
--- backend/lambda/app/core/api_endpoints.py
+++
@@ -1,4 +0,0 @@
-from enum import Enum
-
-class ApiEndpoints:
-    endpoints = ['/addContactForUser', '/deleteUser', '/getContactById', '/getContactsForUser', '/getTagsForUser', '/getUserDetails', '/removeContactForUser', '/searchContacts', '/storeUserCredentials', '/updateContactForUser', '/validateUserCredentials']
