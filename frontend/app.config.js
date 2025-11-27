b1a318531ae7b36e6d2169351985c36060acdf4a
Josh Silverberg <joshsiverberg8@gmail.com>
2025-11-27 13:30:29 -0500

    Register image picker permissions and update location permissions

--- frontend/app.config.js
+++ frontend/app.config.js
@@ -41,18 +41,18 @@ export default {
       "expo-router",
       [
       "expo-location",
-        {
-          "locationAlwaysAndWhenInUsePermission": "We use your location to show nearby contacts on the map and help you discover people in your area. Your location is only used while the app is open.",
-          "locationWhenInUsePermission": "We use your location to show nearby contacts on the map while you're using the app.",
-          "isIosBackgroundLocationEnabled": false
-        }
-      ]
+      {
+        "locationAlwaysAndWhenInUsePermission": "We use your location to show nearby contacts on the map and help you discover people in your area. Your location is only used while the app is open.",
+        "locationWhenInUsePermission": "We use your location to show nearby contacts on the map while you're using the app.",
+        "isIosBackgroundLocationEnabled": false
+      }
+      ],
       [
       "expo-image-picker",
-        {
-          "photosPermission": "We need access to your photo library so you can select profile pictures for your contacts and personalize your account.",
-          "cameraPermission": "We need access to your camera so you can take profile pictures for your contacts and account."
-        }
+      {
+        "photosPermission": "We need access to your photo library so you can select profile pictures for your contacts and personalize your account.",
+        "cameraPermission": "We need access to your camera so you can take profile pictures for your contacts and account."
+      }
       ],
       "expo-font",
       "expo-secure-store",
